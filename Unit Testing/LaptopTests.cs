75742ec1a37223470a46350865e2a1efa888aac2
The_Lump <thelump@lump.com>
2021-07-07 16:09:58 -0400

    finished Laptop Unit testing

--- Unit Testing/LaptopTests.cs
+++ Unit Testing/LaptopTests.cs
@@ -6,6 +6,31 @@ namespace Unit_Testing
     [TestClass]
     public class LaptopTests
     {
+        
+        Laptop test_laptop = new Laptop("laptop", "samsung", "linux");
 
+        [TestMethod]
+        public void ConstructorSetsBatteryLevel()
+        {
+            Assert.AreEqual(100, test_laptop.BatteryLevel);
+        }
+        [TestMethod]
+        public void TestBatteryChange()
+        {
+            test_laptop.CheckBatteryLevel();
+            Assert.AreEqual(95, test_laptop.BatteryLevel);
+        }
+        [TestMethod]
+        public void CheckIP()
+        {
+            Assert.AreEqual("10.000.000.1", test_laptop.IP);
+        }
+        [TestMethod]
+        public void CheckSendEmail()
+        {
+            string str = test_laptop.SendEmail("Joe", "Wazzzzzup");
+            Assert.AreEqual("Email sent to Joe, the message was Wazzzzzup", str);
+        }
+            
     }
 }
